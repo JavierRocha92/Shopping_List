552ecdfd14e9333800a59af409bf5733309e1b1f
Javier Rocha Gómez <javierrg1992@gmail.com>
2023-09-29 16:22:21 +0200

    responsive list, button + and - focus and blur

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -13,8 +13,10 @@ const createElement =(name) =>{
     list.innerHTML += '<li class="listitem">'+
     '<span class="listitem__name" id="'+price+'">'+name+'</span>'+
     '<span class="listitem__price">'+price+'.00 €</span>'+
+    '<div class="buttons">'+
     '<button class="listbtn__add">+ </button>'+
     '<button class="listbtn__decrease"> - </button>'+
+    '</div>'+
     '<span class="listitem__amount">1</span>'+
     '<span class="listbtn__erase">x</span>'+
     '</li>'
@@ -59,27 +61,33 @@ const amountPurchase = () =>{
 const modifyProduct = (event) =>{
     const e = event.target
     //GUARDAMOS EN UN ARRAY LOS ELEMENTOS HIJOS DE EL ELEMENTO CAPTURADO EN ESTE CASO EL <li>
-    const listElement = e.parentNode.childNodes
+    const listElement = e.parentNode.parentNode.childNodes
+    console.log(e.parentNode.parentNode)
     //FILTRAMOS DEL BOTON QUE SE HA PULSADO MEDIANTE SU CLASE
     switch(e.className){
         case 'listbtn__add':
             //CON ESTE BOTON AÑADIMOS UNIDADES 
-            listElement[4].textContent = parseInt(listElement[4].textContent) + 1
+            listElement[3].textContent = parseInt(listElement[3].textContent) + 1
             //GUARDAMOS EL PRECIO NUEVO RESPECTO A LAS UNIDADES QUE TIENE EL ARTICULO EN LA LIST
-            parent[1].textContent = (parseInt(parent[4].textContent) * parseInt(parent[0].id)) + '.00 €'
+            listElement[1].textContent = (parseInt(listElement[3].textContent) * parseInt(listElement[0].id)) + '.00 €'
+            e.focus()
             break;
-        case 'listbtn__decrease':
-            //CON ESTE BOTON RESTAMOS UNIDADES Y COMPROBAMOS SI LAS UNIDADES SON UNO PARA NO LLEGAR A 0 EN LA INTERFAZ
-            if(listElement[4].textContent != 1)
-                listElement[4].textContent = parseInt(listElement[4].textContent) - 1
-                ///GUARDAMOS EL PRECIO NUEVO RESPECTO A LAS UNIDADES QUE TIENE EL ARTICULO EN LA LIST
-                parent[1].textContent = (parseInt(parent[4].textContent) * parseInt(parent[0].id)) + '.00 €'                
+            case 'listbtn__decrease':
+                //CON ESTE BOTON RESTAMOS UNIDADES Y COMPROBAMOS SI LAS UNIDADES SON UNO PARA NO LLEGAR A 0 EN LA INTERFAZ
+                if(listElement[3].textContent != 1)
+                    listElement[3].textContent = parseInt(listElement[3].textContent) - 1
+                    ///GUARDAMOS EL PRECIO NUEVO RESPECTO A LAS UNIDADES QUE TIENE EL ARTICULO EN LA LIST
+                    listElement[1].textContent = (parseInt(listElement[3].textContent) * parseInt(listElement[0].id)) + '.00 €'                
+                    e.focus()
             break;
         case 'listbtn__erase':
             //CON ESTE BOTON BORRAMOS EL ELEMENTO DE LA LISTA HACIENDO REFENCIA AL PADRE QUE ES EL <li>
             e.parentNode.remove()
             break;
-    }
+        }
+        e.addEventListener('mouseup',()=>{
+            e.blur()
+    })
     
 
     /* COGEMOS LA INFORMACION DEL PRECIO DEL PRODUCTO GUATDANDOLO EN UN ID. HABRIA QUE BUSCA OTRA FORMA MSD CORRECTA DE GUARDARLO */
@@ -94,4 +102,4 @@ const changeFocus = () => {
 /* LISTA DE EVENTOS */
 btnaniadir.addEventListener('click',addItem)
 btncomprar.addEventListener('click',amountPurchase)
-list.addEventListener('click',modifyProduct)
+list.addEventListener('mousedown',modifyProduct)
